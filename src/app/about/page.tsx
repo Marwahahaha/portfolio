5ccbc32d247c0dcd40cec212888dcbbb6ad440df
Marwah.Al-Shakhli <marwah.al-shakhli@student.howest.be>
2025-10-26 18:33:18 +0100

    Add About page and profile image to the portfolio

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black p-8">
+            <h1 className="text-3xl font-bold mb-6 dark:text-zinc-50">
+                About Me
+            </h1>
+            <div className="prose dark:prose-invert max-w-3xl">
+                <p>Hi! My name is Marwah and I'm a software engineering student</p>
+            </div>
+        </div>
+    );
+}
